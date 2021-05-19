9a1676440cf577ae5f39178cab34e60d1181639b
liugu1983 <86495338@qq.com>
2021-05-19 01:41:40 -0700

    add a simple main

--- snake/main.cpp
+++ snake/main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include "arenamap.h"
+#include "snake.h"
 using namespace std;
 int main(int argc, char *argv[])
 {
+    arenamap Map(20,30);
+    snake S1(Map);
+
+    /*ncurses gui start*/
+    initscr();
+    Map.print_nc();
+    endwin();
     return 0;
 }
